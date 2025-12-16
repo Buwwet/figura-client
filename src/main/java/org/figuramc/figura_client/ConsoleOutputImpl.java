5e999c45239c86371f9fb38bc6d6007663884dc2
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2025-12-16 10:20:28 -0800

    add logNativeFormatted implementation

--- src/main/java/org/figuramc/figura_client/ConsoleOutputImpl.java
+++ src/main/java/org/figuramc/figura_client/ConsoleOutputImpl.java
@@ -1,10 +1,23 @@
 package org.figuramc.figura_client;
 
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.datafixers.util.Pair;
+import com.mojang.serialization.DataResult;
+import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.NbtOps;
+import net.minecraft.nbt.SnbtGrammar;
+import net.minecraft.nbt.Tag;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.ComponentContents;
+import net.minecraft.network.chat.ComponentSerialization;
+import net.minecraft.util.parsing.packrat.commands.CommandArgumentParser;
 import org.figuramc.figura_core.minecraft_interop.ConsoleOutput;
 import org.figuramc.figura_core.text.FormattedText;
 import org.figuramc.figura_core.util.exception.FiguraException;
 
 public class ConsoleOutputImpl implements ConsoleOutput {
+    private static final CommandArgumentParser<Tag> TAG_PARSER = SnbtGrammar.createParser(NbtOps.INSTANCE);
 
     @Override
     public void logSimple(String message) {
@@ -18,10 +31,22 @@ public class ConsoleOutputImpl implements ConsoleOutput {
         throw new AssertionError("Not implemented");
     }
 
+    /**
+     * tellraw/sNBT
+     * equivalent of printJson on 0.1.x
+     */
     @Override
     public void logNativeFormatted(String formatted) {
-        // TODO
-        throw new AssertionError("Not implemented");
+        Component text;
+        try {
+            Tag t = TAG_PARSER.parseForCommands(new StringReader(formatted));
+            DataResult<Pair<Component, Tag>> decode = ComponentSerialization.CODEC.decode(NbtOps.INSTANCE, t);
+            text = decode.getOrThrow().getFirst();
+        } catch (CommandSyntaxException | IllegalStateException ignored) {
+            // fallback to just printing the raw text
+            text = Component.literal(formatted);
+        }
+        Minecraft.getInstance().player.displayClientMessage(text, false);
     }
 
     @Override
